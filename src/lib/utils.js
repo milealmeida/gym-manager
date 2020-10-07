f6c2663d2013dac1861dc605bf0ec7721c5b75fa
Milena <milenaalmeidadev@hotmail.com>
2020-10-07 15:42:52 -0400

    Trabalhando com banco de dados

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
 module.exports = {
-    age: function(timestamp){
+    age(timestamp){
         const today = new Date();
         const birthDate = new Date(timestamp);
    
@@ -18,7 +18,7 @@ module.exports = {
        return age;
     },
     
-    date: function(timestamp){
+    date(timestamp){
         const date = new Date(timestamp);
 
         // yyyy
@@ -36,7 +36,8 @@ module.exports = {
             month,
             year,
             iso: `${year}-${month}-${day}`,
-            birthDay: `${day}/${month}`
+            birthDay: `${day}/${month}`,
+            format: `${day}/${month}/${year}`
         }; []
     }
 }
